eb2d293c7958890715832eb379734b04c66cbd70
LocaCoco0 <166264863+LocaCoco0@users.noreply.github.com>
2024-04-18 14:22:51 -0500

    Add files via upload

---
+++ Exercise-Objects-as-Dictionaries/script.js
@@ -0,0 +1,9 @@
+let dictionary = {
+  egg: "An oviod or round object laid by a chicken.",
+  bacon: "A delicious breakfast side dish",
+  cheese: "A curded and aged a bit of animal milk",
+}
+
+for(let key in dictionary) {
+  console.log(key + " " + dictionary[key]);
+}
