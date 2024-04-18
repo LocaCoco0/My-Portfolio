3280bf79ed6f4d02e9e87e5a7a731d5558dc3a71
LocaCoco0 <166264863+LocaCoco0@users.noreply.github.com>
2024-04-18 14:32:39 -0500

    Add files via upload

---
+++ Saying-Hi-Starter/script.js
@@ -0,0 +1,23 @@
+// console.log("Hi, Billy Bob!");
+// console.log("Hi, Gunther!");
+// console.log("Hi, Susan!");
+
+// console.log(STUDENTS)
+
+// FORWARDS FOR LOOp
+// for (let i = 0; i < STUDENTS.length; i++) {
+//   console.log(`Hi, ${STUDENTS[i]}!`);
+// }
+
+// FOWARDS WHILE LOOP
+// let i = 0
+
+// while (i < STUDENTS.length) {
+//   console.log(`Hi, ${STUDENTS[i]}!`);
+//   i++;
+// }
+
+// BACKWARDS FOR LOOP
+for (let i = STUDENTS.length - 1; i >= 0; i--) {
+  console.log(`Hi, ${STUDENTS[i]}!`);
+}
