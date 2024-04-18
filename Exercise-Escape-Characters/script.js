8ddda5af6b88e6e9a35683e635419bc933d21b63
LocaCoco0 <166264863+LocaCoco0@users.noreply.github.com>
2024-04-18 14:46:27 -0500

    Add files via upload

---
+++ Exercise-Escape-Characters/script.js
@@ -0,0 +1,20 @@
+// new line 
+console.log('T\nA\nY\nL\nO\nR');
+
+// single quotes + apostrophes
+console.log('That\'s so fetch!')
+
+// quotes within strings 
+
+// if inside and outside quotes match in format, all is well
+console.log("In William Shakespeare's Hamlet, it was 'to be, or not to be, that is the question'");
+
+// if inner and outer quotes are the same format, need to escape the inner quotes
+console.log("In the first 21st century,\"to code, or not to code, that is the challenge.\"")
+
+console.log(`Fancy ES6 strings with 
+backticks.
+This is the second line of the quote
+Let's type a line with an apostrophe in it.`)
+
+
