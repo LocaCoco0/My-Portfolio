c6dcd3c75ffeb43578ae2391d7b89cdc47135953
LocaCoco0 <166264863+LocaCoco0@users.noreply.github.com>
2024-04-18 14:45:17 -0500

    Add files via upload

---
+++ Exercise-Returns-vs-Prints/script.js
@@ -0,0 +1,16 @@
+function printAThing(){
+  console.log("A thing.");
+}
+
+// printAThing();
+// printAThing();
+// printAThing();
+// printAThing();
+
+function returnAThing(){
+  return "A thing.";
+}
+
+// let result = returnAThing();
+// console.log(result);
+console.log(returnAThing());
