f75ce91eeb509e06c07bbc5578b97ddf4f8c4cd3
LocaCoco0 <166264863+LocaCoco0@users.noreply.github.com>
2024-04-18 14:36:04 -0500

    Add files via upload

---
+++ goldenratio/script.js
@@ -0,0 +1,19 @@
+// This function should output n numbers in the 
+// fibonacci sequence
+function Fib(n) {
+  let prevFib1 = 0;
+  let prevFib2 = 0;
+  let currentFib = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (i == 0 || i == 1) {
+      currentFib == 1; 
+    } else {
+      currentFib == prevFib 1 + prevFib 2; 
+    }
+
+    console.log(currentFib)
+  } else if (i == 2) {
+    currentFib == 1;
+  }
+}
